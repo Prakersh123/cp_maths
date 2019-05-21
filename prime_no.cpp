dbdba6aa8ca4693b4e93187a969f6481f79383ef
Prakersh123 <prakersh009@gmail.com>
2019-05-21 05:36:57 +0530

    code for prime number generation

--- prime_no.cpp
+++ prime_no.cpp
@@ -23,7 +23,7 @@ using namespace std;
         return true;
     }
 
-        //sieve of erathosense O()
+        //sieve of erathosense O(nlog(n)log(n)) sc O(n) 
     void sieve(int arr[],int n)
     {
         for(int i=2;i*i<=n;i++)
@@ -37,16 +37,38 @@ using namespace std;
             }
         }
     }
-int main()
-{
+       //here we just used bitset to optimize the space. 
+    void bitset_sieve()
+    {
         int n;
         cin>>n;
-        int prime[n+1];
-        for(int i=0;i<=n;i++)prime[i]=1;
-        sieve(prime,n);
+        bitset<n+1> arr;
+        arr.set();
+        arr[0]=arr[1]=0;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(arr[i]==1){
+                 //as our i is prime no , so now we will unmark all those no which are divisible by this i
+                for(int j=i*i;j<=n;j+=i)
+                {
+                    arr[j]=0;
+                }
+            }
+        }
         for(int i=2;i<=n;i++)
-        if(prime[i]==1)
+        if(arr[i]==1)
         cout<<i<<" ";
-
+    }
+int main()
+{
+        // int n;
+        // cin>>n;
+        // int prime[n+1];
+        // for(int i=0;i<=n;i++)prime[i]=1;
+        // sieve(prime,n);
+        // for(int i=2;i<=n;i++)
+        // if(prime[i]==1)
+        // cout<<i<<" ";
+        bitset_sieve();
         return 0;
 }
